0c900c831a8dc8a9b6bae2cd12fd7411a3d894eb
Leonardo <Leonardo.Hernandez.-ND@disney.com>
2026-01-12 17:19:29 -0600

    feat(core): implementar sistema de manejo de errores y excepciones

---
+++ lib/core/error/failure_mapper.dart
@@ -0,0 +1,20 @@
+import 'exceptions.dart';
+import 'failures.dart';
+
+class FailureMapper {
+  static Failure mapExceptionToFailure(Exception exception) {
+    if (exception is ServerException) {
+      return ServerFailure(message: exception.message);
+    } else if (exception is CacheException) {
+      return CacheFailure(message: exception.message);
+    } else if (exception is NetworkException) {
+      return NetworkFailure(message: exception.message);
+    } else if (exception is NotFoundException) {
+      return NotFoundFailure(message: exception.message);
+    } else {
+      return ServerFailure(
+          message: 'Error inesperado: ${exception.toString()}');
+    }
+  }
+}
+
