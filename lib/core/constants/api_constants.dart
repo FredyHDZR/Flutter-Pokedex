da9da8377a81ca6a12ffa5cb197c4734e52e0678
Leonardo <Leonardo.Hernandez.-ND@disney.com>
2026-01-12 17:19:18 -0600

    feat(core): agregar constantes de aplicación y API

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'https://pokeapi.co/api/v2/';
+  static const String pokemonEndpoint = '/pokemon';
+  static const String imageBaseUrl =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+}
+
