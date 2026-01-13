8262210471856b199b9df58eb7979d85743095f8
Leonardo <Leonardo.Hernandez.-ND@disney.com>
2026-01-13 12:24:16 -0600

    feat(ui): configurar navegación con go_router y actualizar main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/injection/injection_container.dart' as di;
+import 'ui/routes/app_router.dart';
+import 'ui/cubits/pokemon_list/pokemon_list_cubit.dart';
+import 'ui/cubits/pokemon_detail/pokemon_detail_cubit.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MainApp());
 }
 
@@ -9,10 +16,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => di.sl<PokemonListCubit>(),
+        ),
+        BlocProvider(
+          create: (_) => di.sl<PokemonDetailCubit>(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: appRouter,
+        title: 'Pokédex',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
         ),
       ),
     );
